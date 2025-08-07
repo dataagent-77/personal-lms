94c7cdc58f41ed187c0ea056c45989b93dd5d935
data_agent_77 <dataagent1375@gmail.com>
2025-08-08 00:00:59 +0400

    Create Repository.js

---
+++ backend/models/Repository.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const RepositorySchema = new mongoose.Schema({
+  link: String,
+  category: { type: String, enum: ['Tool', 'Study'] },
+  utilization: { type: String, enum: ['Used', 'Not Used'] },
+  usedInProjects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }]
+});
+module.exports = mongoose.model('Repository', RepositorySchema);
