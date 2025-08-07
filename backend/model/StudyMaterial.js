f4c2dc1799b4efb56dd57d45cae09c3b74dee066
data_agent_77 <dataagent1375@gmail.com>
2025-08-08 00:03:13 +0400

    Delete backend/model directory

--- backend/model/StudyMaterial.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const StudyMaterialSchema = new mongoose.Schema({
-  name: String,
-  link: String,
-  linkedProjects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Project' }]
-});
-
-module.exports = mongoose.model('StudyMaterial', StudyMaterialSchema);
